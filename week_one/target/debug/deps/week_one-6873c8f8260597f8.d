f20fd5eb762d8667dcc1068d5fd40dc1839d4af6
Middo1 <hamidadeshina1@gmail.com>
2025-03-18 21:19:12 +0100

    Let's try this aganin

--- week_one/target/debug/deps/week_one-6873c8f8260597f8.d
+++
@@ -1,5 +0,0 @@
-/home/middo/Codes/rust-assignments/week_one/target/debug/deps/week_one-6873c8f8260597f8: src/main.rs
-
-/home/middo/Codes/rust-assignments/week_one/target/debug/deps/week_one-6873c8f8260597f8.d: src/main.rs
-
-src/main.rs:
--- week_one/target/debug/week_one.d
+++
@@ -1,1 +0,0 @@
-/home/middo/Codes/rust-assignments/week_one/target/debug/week_one: /home/middo/Codes/rust-assignments/week_one/src/main.rs
