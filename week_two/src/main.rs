18c1af612eb7b5c826048f23a17a9a4d0a33a6af
Middo1 <hamidadeshina1@gmail.com>
2025-03-23 21:21:17 +0100

    Chore: week two assignment

---
+++ week_two/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    println!("Hello, world!");
+    
+    /*
+     * A rust program that
+     * stores student Record - at least 10
+     * Calculate the total and average
+     * Determine the highest scoring student
+     * Counts how many students failed - Failure starts around < 50
+     * Sorts the Student by pass and fail
+     * Deadline Monday 4:00pm
+     */
+}
+struct Student {
+    name: str,
+    score: u32
+}
+
+fn create_students(student_names: [&str;10], student_scores: [u32;10]) {
+    let mut i = 0;
+    let list_of_students = [];
+    while i < student_names.len() {
+        
+    }
+}
